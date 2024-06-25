b8f40a8757d021f3782365f5663b2e46a70f3761
BuhleDamane <ziziphodamane@gmail.com>
2024-06-25 23:17:56 +0200

    updated my files

--- src/heading.js
+++ src/heading.js
@@ -19,7 +19,13 @@ import { FaFacebookSquare } from "react-icons/fa";
 import { RiStarSFill } from "react-icons/ri";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaPaperPlane } from "react-icons/fa";
+import { FaTimeline } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+import { BsDashLg } from "react-icons/bs";
 import"./../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import logoImage from './image.js/logo2.png'
 import coverImage from './image.js/cover 2.png'
@@ -207,6 +213,41 @@ export default function Heading(){
         </div>
         </div>
   <div className="contact-page" id="contact">
+    <div className="top-footer">
+    <h1 class="sub-title">
+    Contact Us
+</h1>
+    <div class="container">
+       <div class="row">
+        <div class="col contact-left">
+<h4>Let us be of service to you!</h4>
+<p><span className="contact-icons"><FaPaperPlane /></span>  ziziphodamane@gmail.com</p>
+<p><span className="contact-icons"><FaPhoneAlt /></span>  (+27) 83 483 1795</p>
+<p> 
+<span className="contact-icons"><FaTimeline /></span>  Lines are open:
+<div>
+      <span><BsDashLg /></span> Monday to Friday :  08:00 - 18:00
+      <br />
+      <span><BsDashLg /></span> Saturday & Sunday : 09:00 - 16:00
+    </div> 
+</p>
+<p> <span className="contact-icons"><FaHome /></span> 5th Street Atrium on 5th 9th Floor Sandton 2196, South Africa</p>
+
+
+        </div>
+        <div class="col contact-right">
+            <form name="submit-to-google-sheet">
+                <input type="text" name="Name" placeholder="Your name" required />
+                <input type="number" name="Email" placeholder="Your phone number" required />
+                <input type="email" name="Email" placeholder="Your email" required />
+                <textarea name="Message" row="6" placeholder="Your message"></textarea>
+           <button type="submit" class="btn btn2 btn3">Submit</button>
+            </form>
+            <span id="msg"></span>
+        </div>
+       </div> 
+    </div>
+</div>
     <div className="middle-footer">
         <div className="bottom-logo">
         <img src={lbImage} className="lb d-none d-md-block d-lg-block d-xl-block" alt="logo"/>
@@ -281,14 +322,18 @@ export default function Heading(){
         <li> <a href="#home"><FaLinkedin /></a></li>
         <li> <a href="#about"><FaFacebookSquare /></a></li>
         <li> <a href="#services"><FaInstagramSquare /></a></li>
-        <li> <a href="#contact"><FaSquareXTwitter /> </a></li>
+        <li> <a href="#contact"><FaWhatsapp />  </a></li>
     </ul>
     </div>
     </div>
   </div>
+  <div class="copyright">
+        <p>Copyright © EduApply. Made with <span className="heart"><FaHeart />  </span> by Buhle Damane.</p>
+    </div>
         </div>
+  
    
- 
+
   
     );
 };
